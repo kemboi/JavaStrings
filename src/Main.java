615a2a55da3b97cab703dac989f2e641a367bb61
EK <evanskemb@gmail.com>
2022-12-27 18:31:48 +0300

    Added StringBuilder
    Improved version

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,17 @@ public class Main {
         // Strings are immutable and a small program can create numerous String objects
         // This code creates 27 string objects because in each iteration a String object becomes eligable for garbage collection.
         String alpha=""; // empty string instantiated
-        for (char current= 'a'; current<='z';current++){
+        for (char current= 'a'; current<='z';current++)
             alpha+=current; // appends an "a"
-            System.out.println(alpha);
-        }
+        System.out.println(alpha);
+
+        // the above code can be improved by using StringBuilder
+        // StringBuilder is not immutable. In each iteration, an interim String value is not created, but appended to the current String object
+        System.out.println("Testing StringBuilder\n\n");
+        StringBuilder alpha2= new StringBuilder();
+        for (char current= 'a'; current<='z';current++)
+            alpha2.append(current); // appends an "a"
+        System.out.println(alpha);
 
     }
 }
