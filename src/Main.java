cfb62823fc291d4e047ad006b63b8a28584c31fc
kemboi <evanskemb@gmail.com>
2022-12-27 18:20:48 +0300

    Update Main.java
    Added an explanation about Java String objects being immutable

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // Strings are immutable and a small program can create numerous String objects
+        // This code creates 27 string objects because in each iteration a String object becomes eligable for garbage collection.
         String alpha=""; // empty string instantiated
         for (char current= 'a'; current<='z';current++){
             alpha+=current; // appends an "a"
